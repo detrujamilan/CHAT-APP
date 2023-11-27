9a22f627f115ec2fd02a95915061668ffee211b0
detrujamilan <milandetruja2@gmail.com>
2023-11-27 17:55:31 +0530

    modules install

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./Screen/Main";
-// import Login from "./components/Login";
-// import SignIn from "./components/SignIn";
+import Login from "./components/Login";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div>
-      <Main/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/DashBoard" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -15,19 +15,16 @@ function SignIn() {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
+                <div className="space-y-6" >
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                             Email address
                         </label>
                         <div className="mt-2">
                             <input
-                                id="email"
-                                name="email"
                                 type="email"
                                 autoComplete="email"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border pl-2 py-1.5 text-gray-900 shadow-sm  focus-within:outline-none placeholder:text-gray-400   sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -40,25 +37,19 @@ function SignIn() {
                         </div>
                         <div className="mt-2">
                             <input
-                                id="password"
-                                name="password"
                                 type="password"
                                 autoComplete="current-password"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border py-1.5 pl-2 text-gray-900 shadow-sm  focus-within:outline-none placeholder:text-gray-400   sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
 
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Sign in
-                        </button>
-                    </div>
-                </form>
+                    <button
+                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Sign in
+                    </button>
+                </div>
 
             </div>
         </div>
